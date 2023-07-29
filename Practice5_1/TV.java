9b41f951d37bee8dab84563a3391783b4ce1b6ac
dasol <121621478+DasolIm@users.noreply.github.com>
2023-07-29 20:50:05 +0900

    Add files via upload

---
+++ Practice5_1/TV.java
@@ -0,0 +1,7 @@
+package Practice5_1;
+
+class TV {
+    private int size;
+    public TV(int size) {this.size = size;}
+    protected int getSize() {return size;}
+}
---
+++ Practice5_1/main5_1.java
@@ -0,0 +1,8 @@
+package Practice5_1;
+
+public class main5_1 {
+public static void main(String[] args) {
+        ColorTV myTV = new ColorTV(32, 1024);
+        myTV.printProperty();
+    }
+}
